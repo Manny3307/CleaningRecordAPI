7d6ce552549babb32c0973f31c90d354a73ba04f
Manny3307 <manmeetarora@gmail.com>
2021-11-19 15:32:54 +1100

    Added list ingredients feature

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from core import models
@@ -51,3 +52,12 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(tag), tag.name)
+    
+    def test_ingredient_str(self):
+        #Test the ingredient strinhg representation
+        ingredient = models.Ingredient.objects.create(
+            user = sample_user(),
+            name = 'cucumber'
+        )
+
+        self.assertEqual(str(ingredient), ingredient.name)
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import authentication_classes, permission_classes
 from rest_framework.permissions import IsAuthenticated
-from core.models import Tag
+from core.models import Tag, Ingredient
 from recipe import serializers
 
 class TagViewSets(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
@@ -11,6 +11,21 @@ class TagViewSets(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateM
     permission_classes = (IsAuthenticated,)
     queryset = Tag.objects.all()
     serializer_class = serializers.TagSerializer
+
+    def get_queryset(self):
+        #Return Objects for the current authenticated user only
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        #Create a new tag
+        serializer.save(user=self.request.user)
+
+class IngredientViewSets(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
+    #Manage Ingredient in the Database
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Ingredient.objects.all()
+    serializer_class = serializers.IngredientSerializer
 
     def get_queryset(self):
         #Return Objects for the current authenticated user only
