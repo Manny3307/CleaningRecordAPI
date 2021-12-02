071fa8f6029d16074c194cf1020640a81ad815eb
Manny3307 <manmeetarora@gmail.com>
2021-12-03 00:26:58 +1100

    Fixed Database functionality

--- app/cleaning_rec/serializers.py
+++ app/cleaning_rec/serializers.py
@@ -1,5 +1,6 @@
+from django.db.models import fields
 from rest_framework import serializers
-from core.models import UberDriver, CreateCleaningRecords
+from core.models import UberDriver, CreateCleaningRecords, UberCleaningRecords
 
 class UberDriverSerializer(serializers.ModelSerializer):
     '''Serializer for Uber Driver Objects'''
@@ -16,3 +17,12 @@ class CreateRecordsSerializer(serializers.ModelSerializer):
         model = CreateCleaningRecords
         fields = ('folder_id', 'folder_name')
         read_only_fields = ('folder_id',)
+
+class TransferCleaningRecordsSerializer(serializers.ModelSerializer):
+    '''Serializer to transfer the cleaning records to main tables in database'''
+
+    class Meta:
+        model = UberCleaningRecords
+        fields = ('id', 'driver_id', 'driver_certificate_id', 'date_and_time_of_trip', 
+                  'date_and_time_of_clean', 'passenger_high_touch_surfaces','driver_high_touch_surfaces')
+        read_only_fields = ('id',)
